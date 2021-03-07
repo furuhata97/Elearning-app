2598f4ba9aff1bdd13e34bfb78e3ee06c31d5fe2
Gustavo Furuhata <gustavo_furuhata@hotmail.com>
2021-03-07 20:39:20 -0300

    :lipstick: add splash screen and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,32 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from 'react-native-splash-screen';
 
 import AppProvider from './hooks';
 
 import Routes from './routes';
 
-const App: React.FC = () => (
-  <NavigationContainer>
-    <StatusBar barStyle="light-content" backgroundColor="#6548A3" translucent />
-    <AppProvider>
-      <View style={{ flex: 1, backgroundColor: '#6548A3' }}>
-        <Routes />
-      </View>
-    </AppProvider>
-  </NavigationContainer>
-);
+const App: React.FC = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <NavigationContainer>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="#6548A3"
+        translucent
+      />
+      <AppProvider>
+        <View style={{ flex: 1, backgroundColor: '#6548A3' }}>
+          <Routes />
+        </View>
+      </AppProvider>
+    </NavigationContainer>
+  );
+};
 
 export default App;
